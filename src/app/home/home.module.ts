348b67171038d1d1df6073207a7f9be243f708a0
manoj020218 <manoj020218@gmail.com>
2023-11-29 00:13:57 +0530

    banner

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { BannerComponent } from '../components/banner/banner.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
+    BannerComponent,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
@@ -7,8 +9,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   show = true;
+  home_top_banner:any[] = [];
+  myForm!: FormGroup;
 
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+
+  ngOnInit() {
+    this.callBanner();
+  }
+
+queryForm(){
+  this.myForm = this.formBuilder.group({
+    // Define your form controls and their validation rules
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    query: ['', [Validators.required, Validators.minLength(6)]],
+  });
+}
+
+customCounterFormatter(inputLength: number, maxLength: number) {
+  return `${maxLength - inputLength} characters remaining`;
+}
+
+onSubmit() {
+  if (this.myForm.valid) {
+    // Form is valid, handle the submission logic
+    console.log('Form submitted:', this.myForm.value);
+  }
+}
 
 
   hide() {
@@ -40,4 +70,29 @@ export class HomePage {
     
   }
 
+
+  callBanner(){
+
+    for (let i = 0; i <5; i++) {
+            // make array of image objects
+            this.home_top_banner.push(
+              {banner:'https://picsum.photos/id/1/200/300'}
+            );
+          };
+
+    // this.bannerApi.getImageByGymId("default_memberlist_page").subscribe({            
+    //   next:dat=>{
+    //     console.log(dat)
+    //     for (let i = 0; i <dat.length; i++) {
+    //       // make array of image objects
+    //       this.members_slides.push(
+    //         {banner:this.baseUri+'/images/'+dat[i].image_path}
+    //       );
+    //     };
+    //   },
+    //   error:err=>{ console.log(err)}
+    // });
+  
+   }
+   
 }
